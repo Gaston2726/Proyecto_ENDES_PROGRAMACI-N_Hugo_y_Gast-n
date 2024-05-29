9196d499be4bcc177b264eec06eec34e45d92c09
Hugo <clasehugo2004@gmail.com>
2024-05-29 17:42:58 +0200

    tt

--- Clase_Cancha1.py
+++ Clase_Cancha1.py
@@ -27,23 +27,7 @@ def añadir_a_lista_cancha(cancha):
     lista_cancha.append(cancha)
     print("EN la lista de canchas hay: ",len(lista_cancha))
 
-class Reserva(Cancha):
-    def __init__(self, numero_cancha, deporte, precio, habilitada, numero_reserva, fecha):
-        super().__init__(numero_cancha, deporte, precio, habilitada)
-        self.numero_reserva=numero_cancha
-        self.fecha=fecha
-def crear_reserva(cancha):
-    numero_reserva=int(input("Dime el numero de la reserva: "))
-    fecha=input("Dime para que fecha quieres la reserva: ")
-    reserva = Reserva(numero_reserva, fecha)
-    cancha.lista_reserva.append(reserva)
 
-def comprobar_cliente(cliente):
-    saldo=int(input("Dime tu saldo: "))
-    if saldo <= -2000:
-        print("No puedes reservar: ")
-    else:
-        print("Si puedes reservar")
 
 nueva_cancha = crear_cancha()
 print("Cancha habilitada:", nueva_cancha.habilitada)#dice si esta habilitada o no 
---
+++ Clases_Reserva1.py
@@ -0,0 +1,17 @@
+class Reserva(Cancha):
+    def __init__(self, numero_cancha, deporte, precio, habilitada, numero_reserva, fecha):
+        super().__init__(numero_cancha, deporte, precio, habilitada)
+        self.numero_reserva=numero_cancha
+        self.fecha=fecha
+def crear_reserva(cancha):
+    numero_reserva=int(input("Dime el numero de la reserva: "))
+    fecha=input("Dime para que fecha quieres la reserva: ")
+    reserva = Reserva(numero_reserva, fecha)
+    cancha.lista_reserva.append(reserva)
+
+def comprobar_cliente(cliente):
+    saldo=int(input("Dime tu saldo: "))
+    if saldo <= -2000:
+        print("No puedes reservar: ")
+    else:
+        print("Si puedes reservar")
