9249d3e89bfc363932080727c53ded03212ef6fd
Hugo <clasehugo2004@gmail.com>
2024-05-30 21:23:49 +0200

    empiezo0

--- Clases_Reserva1.py
+++ Clases_Reserva1.py
@@ -1,19 +1,60 @@
-class Reserva():
+class Reserva:
     def __init__(self, numero_cancha, deporte, precio, habilitada, numero_reserva, fecha):
-        super().__init__(numero_cancha, deporte, precio, habilitada)
-        self.numero_reserva=numero_cancha
-        self.fecha=fecha
-def crear_reserva(cancha):
-    numero_reserva=int(input("Dime el numero de la reserva: "))
-    fecha=input("Dime para que fecha quieres la reserva: ")
-    reserva = Reserva(numero_reserva, fecha)
+        self.numero_cancha = numero_cancha
+        self.deporte = deporte
+        self.precio = precio
+        self.habilitada = habilitada
+        self.numero_reserva = numero_reserva
+        self.fecha = fecha
+
+    def comprobar_cliente(self):
+        saldo = int(input("Dime tu saldo: "))
+        if saldo <= -2000:
+            print("No puedes reservar.")
+            return False
+        else:
+            print("Puedes reservar.")
+            return True
+
+def crear_reserva(cancha, numero_reserva, fecha):
+    if not cancha.habilitada:
+        print("La cancha no está habilitada para reservas.")
+        return
+    if cancha.lista_reserva:
+        print("La cancha ya tiene una reserva.")
+        return
+    reserva = Reserva(cancha.numero_cancha, cancha.deporte, cancha.precio, cancha.habilitada, numero_reserva, fecha)
     cancha.lista_reserva.append(reserva)
+    print("Reserva creada exitosamente.")
 
-def comprobar_cliente(cliente):
-    saldo=int(input("Dime tu saldo: "))
-    if saldo <= -2000:
-        print("No puedes reservar: ")
-    else:
-        print("Si puedes reservar")
+# Suponiendo que `nueva_cancha` ya está definida y habilitada
+numero_reserva = int(input("Dime el número de la reserva: "))
+fecha_reserva = input("Dime para qué fecha quieres la reserva: ")
+crear_reserva(nueva_cancha, numero_reserva, fecha_reserva)
 
-crear_reserva(nueva_cancha)
+def menu():
+    while True:
+        print("\nOpciones:")
+        print("1. Crear reserva")
+        print("2. Listar reservas de una cancha")
+        print("3. Registrar pago del cliente")
+        print("4. Salir")
+        opcion = input("Elige una opción: ")
+
+        if opcion == "1":
+            numero_reserva = int(input("Dime el número de la reserva: "))
+            fecha_reserva = input("Dime para qué fecha quieres la reserva: ")
+            crear_reserva(nueva_cancha, numero_reserva, fecha_reserva)
+        elif opcion == "2":
+            nueva_cancha.listar_reservas()  # Suponiendo que tienes un método 'listar_reservas' en la clase Cancha
+        elif opcion == "3":
+            # Suponiendo que 'cliente' es una instancia de la clase Cliente
+            monto_pago = float(input("Ingrese el monto del pago: "))
+            cliente.aumentar_saldo(monto_pago)  # Suponiendo que tienes un método 'aumentar_saldo' en la clase Cliente
+            print("Pago registrado exitosamente.")
+        elif opcion == "4":
+            break
+        else:
+            print("Opción no válida. Por favor, elige nuevamente.")
+
+menu()
